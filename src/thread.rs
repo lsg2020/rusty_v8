d1a86458d8daaa32c2f59e0285c94dfc635dc3a9
2468180623@qq.com <2468180623@qq.com>
2020-12-19 12:36:55 +0000

    support stepnext async function

--- src/thread.rs
+++ src/thread.rs
@@ -3,26 +3,28 @@ use std::mem::MaybeUninit;
 use crate::isolate::Isolate;
 
 extern "C" {
-    fn v8__Locker__CONSTRUCT(buf: *mut MaybeUninit<Locker>, isolate: *mut Isolate);
-    fn v8__Locker__DESTRUCT(this: *mut Locker);
+    fn v8__Locker__CONSTRUCT(buf: *mut MaybeUninit<Locker>, isolate: *mut Isolate, custom_archive: *mut libc::c_void);
+    fn v8__Locker__DESTRUCT(this: *mut Locker, custom_archive: *mut libc::c_void);
 }
 
 #[repr(C)]
-pub struct Locker([usize; 2]);
+pub struct Locker([usize; 2], *mut libc::c_void);
 
 impl Locker {
-    pub fn new(isolate: &mut Isolate) -> Self {
+    pub fn new(isolate: &mut Isolate, custom_archive: *mut libc::c_void) -> Self {
         let mut locker: MaybeUninit<Self> = MaybeUninit::uninit();
         unsafe {
-            v8__Locker__CONSTRUCT(&mut locker, isolate);
-            locker.assume_init()
+            v8__Locker__CONSTRUCT(&mut locker, isolate, custom_archive);
+            let mut l = locker.assume_init();
+            l.1 = custom_archive;
+            l
         }
     }
 }
 
 impl Drop for Locker {
     fn drop(&mut self) {
-        unsafe { v8__Locker__DESTRUCT(self) };
+        unsafe { v8__Locker__DESTRUCT(self, self.1) };
     }
 }
 
